76a30e46490b35eb38bb0e5ce76417bb62e8cd2d
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-03-17 18:31:43 +0000

    Add graticules, lat/long lines every 10 deg

--- src/components/worldmap/Marks.js
+++ src/components/worldmap/Marks.js
@@ -1,20 +1,25 @@
-import { geoNaturalEarth1, geoPath } from 'd3'
+import { geoNaturalEarth1, geoPath, geoGraticule } from 'd3'
 
 const projection = geoNaturalEarth1()
 const path = geoPath(projection)
+const graticule = geoGraticule()
+
+const randKey = () => Math.random(0, 10000)
 
 export const Marks = ({ data: { land, interiors } }) => (
   <g className="mapmarks">
+    {/* Creates the sphere */}
     <path className="sphere" d={path({ type: 'Sphere' })} />
+
+    {/* latitude and longtiude lines very 10 degrees */}
+    <path className="graticules" key={randKey} d={path(graticule())} />
+
+    {/* projects the land features */}
     {land.features.map((feature) => (
-      <path className="land" key={Math.random(0, 10000000)} d={path(feature)} />
+      <path className="land" key={randKey} d={path(feature)} />
     ))}
-    {
-      <path
-        className="interiors"
-        key={Math.random(0, 10000000)}
-        d={path(interiors)}
-      />
-    }
+
+    {/* Shades the interior country borders, not ocean/sea adjacent */}
+    <path className="interiors" key={randKey} d={path(interiors)} />
   </g>
 )
