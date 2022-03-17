3c8c3c19b72844bd9694b4f71b08e26ddd7c42c2
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-03-17 09:58:29 +0000

    first display of worldmap

--- src/components/worldmap/Marks.js
+++ src/components/worldmap/Marks.js
@@ -1,33 +1,12 @@
-import { curveNatural } from 'd3'
-import { line } from 'd3'
-export const Marks = ({
-  data,
-  yScale,
-  xScale,
-  xValue,
-  yValue,
-  toolTipFormat,
-  circleRadius = 10,
-}) => (
-  <g className="marks">
-    <path
-      fill="none"
-      stroke="black"
-      d={line()
-        .x((d) => xScale(xValue(d)))
-        .y((d) => yScale(yValue(d)))
-        .curve(curveNatural)(data)}
-    />
-    {data.map((d) => (
-      <circle
-        className="mark"
-        key={Math.random(0, 100000)}
-        cx={xScale(xValue(d))}
-        cy={yScale(yValue(d))}
-        r={circleRadius}
-      >
-        <title>{toolTipFormat(xValue(d))}</title>
-      </circle>
+import { geoEqualEarth, geoPath } from 'd3'
+
+const projection = geoEqualEarth()
+const path = geoPath(projection)
+
+export const Marks = ({ data }) => (
+  <g className="marks" key={data.features.indexOf}>
+    {data.features.map((feature) => (
+      <path d={path(feature)} />
     ))}
   </g>
 )
--- src/worldmap.js
+++ src/worldmap.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import { scaleLinear, scaleTime, timeFormat, extent } from 'd3'
+
 import './styles/global.css'
 import { useData } from './components/worldmap/useData'
-import { AxisBottom } from './components/worldmap/AxisBottom'
-import { AxisLeft } from './components/worldmap/AxisLeft'
+
 import { Marks } from './components/worldmap/Marks'
 
 const jsonUrl = process.env.REACT_APP_WORLDMAP_DATA
 
 const width = 960
 const height = 500
-const margin = { top: 50, right: 30, bottom: 65, left: 90 }
-const xAxisLabelOffset = 50
-const yAxisLabelOffset = 40
 
 const Worldmap = () => {
   const data = useData(jsonUrl)
@@ -21,65 +17,9 @@ const Worldmap = () => {
     return <pre>Loading...</pre>
   }
 
-  const innerHeight = height - margin.top - margin.bottom
-  const innerWidth = width - margin.right - margin.left
-
-  const xValue = (d) => d.timestamp
-  const xAxisLabel = 'Time'
-
-  const yValue = (d) => d.temperature
-  const yAxisLabel = 'Temperature'
-
-  const xAxisTickFormat = timeFormat('%a')
-
-  const xScale = scaleTime()
-    .domain(extent(data, xValue))
-    .range([0, innerWidth])
-    .nice()
-
-  const yScale = scaleLinear()
-    .domain(extent(data, yValue))
-    .range([innerHeight, 0])
-    .nice()
-
   return (
     <svg width={width} height={height}>
-      <g transform={`translate(${margin.left}, ${margin.top})`}>
-        <AxisBottom
-          xScale={xScale}
-          innerHeight={innerHeight}
-          tickFormat={xAxisTickFormat}
-          tickoffset={7}
-        />
-        <AxisLeft yScale={yScale} innerWidth={innerWidth} tickoffset={7} />
-        <text
-          className="axis-label"
-          textAnchor="middle"
-          x={innerWidth / 2}
-          y={innerHeight + xAxisLabelOffset}
-        >
-          {xAxisLabel}
-        </text>
-
-        <text
-          className="axis-label"
-          textAnchor="middle"
-          transform={`translate(${-yAxisLabelOffset},${
-            innerHeight / 2
-          }) rotate(-90)`}
-        >
-          {yAxisLabel}
-        </text>
-        <Marks
-          data={data}
-          yScale={yScale}
-          xScale={xScale}
-          xValue={xValue}
-          yValue={yValue}
-          toolTipFormat={xAxisTickFormat}
-          circleRadius={3}
-        />
-      </g>
+      <Marks data={data} />
     </svg>
   )
 }
